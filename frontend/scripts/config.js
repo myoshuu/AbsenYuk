cf2221118c133443ebfc4211f641507e034f105b
Muhammad Putra Ramadhan <2410010262@uniska-bjm.ac.id>
2026-05-20 23:05:49 +0800

    updated baseurl

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -9,19 +9,19 @@
 
 const API_CONFIG = {
   // Base URL untuk API backend
-  API_BASE_URL: 'http://localhost:3000/api',
-  
+  API_BASE_URL: 'http://localhost:5143/api',
+
   // Endpoint untuk login
   LOGIN_ENDPOINT: '/user/login',
-  
+
   // Endpoint untuk register
   REGISTER_ENDPOINT: '/user/register',
-  
+
   // Get full login URL
   getLoginUrl() {
     return this.API_BASE_URL + this.LOGIN_ENDPOINT;
   },
-  
+
   // Get full register URL
   getRegisterUrl() {
     return this.API_BASE_URL + this.REGISTER_ENDPOINT;
